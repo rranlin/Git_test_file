782857ad7c97b1476c0d7d5273474c3dd276af78
RaNlin <f04050714@gmail.com>
2019-01-07 07:47:27 +0800

    insert name

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hello world!\n");
+    printf("\nSuch a nice function!\n");
+    printf("\nHave a nice day!!");
+
+    char name[100];
+
+    printf("what's your name ?");
+    scanf("%c",&name);
+    printf("Your name is %c",name);
+
+    return 0;
+}
